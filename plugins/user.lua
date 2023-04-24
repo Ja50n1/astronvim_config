c3df98a33f28489d1ba61101e7beded209b5ca3d
Ja50n1 <jason.dennis.1@gmail.com>
2023-04-24 19:07:54 -0400

    added some plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,22 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+
+  {
+    "folke/todo-comments.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
+    --config = function()
+    --  require("todo-comments").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      -- }
+    -- end,
+    opts = {},
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+    keys = { 
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in TodoTelescope" },
+    },
+  },
 }
